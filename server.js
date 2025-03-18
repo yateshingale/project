6ded0e160f0f6d9f81bd53709671d7c5b9995b5d
Ubuntu <ubuntu@ip-172-31-83-230.ec2.internal>
2025-03-18 18:03:27 +0000

    testing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Use environment variables
 const PORT = process.env.PORT || 3000;
-const MESSAGE = process.env.MESSAGE || "Hello Vicky!";
+const MESSAGE = process.env.MESSAGE || "Hello bunny!";
 
 // Basic route
 app.get('/', (req, res) => {
